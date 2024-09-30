46d76ce84e80dff241e20c9be54415c4e5215843
DoanPhan <doan.phan@silentium.io>
2024-09-30 09:10:20 +0700

    solve: 731

--- 729.my-calendar-i.go
+++ 729.my-calendar-i.go
@@ -1,44 +1,44 @@
 package main
 
-/*
- * @lc app=leetcode id=729 lang=golang
- *
- * [729] My Calendar I
- */
+// /*
+//  * @lc app=leetcode id=729 lang=golang
+//  *
+//  * [729] My Calendar I
+//  */
 
-// @lc code=start
-type MyCalendar struct {
-	booking [][2]int
-}
+// // @lc code=start
+// type MyCalendar struct {
+// 	booking [][2]int
+// }
 
-func Constructor() MyCalendar {
-	return MyCalendar{}
-}
+// func Constructor() MyCalendar {
+// 	return MyCalendar{}
+// }
 
-func (this *MyCalendar) Book(start int, end int) bool {
-	for _, period := range this.booking {
-		bookedStart := period[0]
-		bookedEnd := period[1]
-		if start < bookedEnd && end >= bookedEnd {
-			return false
-		}
-		if start <= bookedStart && end > bookedStart {
-			return false
-		}
-		if start >= bookedStart && end < bookedEnd {
-			return false
-		}
-		if start < bookedStart && end > bookedEnd {
-			return false
-		}
-	}
-	this.booking = append(this.booking, [2]int{start, end})
-	return true
-}
+// func (this *MyCalendar) Book(start int, end int) bool {
+// 	for _, period := range this.booking {
+// 		bookedStart := period[0]
+// 		bookedEnd := period[1]
+// 		if start < bookedEnd && end >= bookedEnd {
+// 			return false
+// 		}
+// 		if start <= bookedStart && end > bookedStart {
+// 			return false
+// 		}
+// 		if start >= bookedStart && end < bookedEnd {
+// 			return false
+// 		}
+// 		if start < bookedStart && end > bookedEnd {
+// 			return false
+// 		}
+// 	}
+// 	this.booking = append(this.booking, [2]int{start, end})
+// 	return true
+// }
 
-/**
- * Your MyCalendar object will be instantiated and called as such:
- * obj := Constructor();
- * param_1 := obj.Book(start,end);
- */
-// @lc code=end
+// /**
+//  * Your MyCalendar object will be instantiated and called as such:
+//  * obj := Constructor();
+//  * param_1 := obj.Book(start,end);
+//  */
+// // @lc code=end
---
+++ 731.my-calendar-ii.go
@@ -0,0 +1,45 @@
+package main
+
+/*
+ * @lc app=leetcode id=729 lang=golang
+ *
+ * [729] My Calendar I
+ */
+
+// @lc code=start
+type MyCalendar struct {
+	booking [][2]int
+}
+
+func Constructor() MyCalendar {
+	return MyCalendar{}
+}
+
+func (this *MyCalendar) Book(start int, end int) bool {
+	count := 0
+	for _, period := range this.booking {
+		bookedStart := period[0]
+		bookedEnd := period[1]
+		if start < bookedEnd && end >= bookedEnd {
+			count++
+		} else if start <= bookedStart && end > bookedStart {
+			count++
+		} else if start >= bookedStart && end < bookedEnd {
+			count++
+		} else if start < bookedStart && end > bookedEnd {
+			count++
+		}
+		if count == 1 {
+			return false
+		}
+	}
+	this.booking = append(this.booking, [2]int{start, end})
+	return true
+}
+
+/**
+ * Your MyCalendar object will be instantiated and called as such:
+ * obj := Constructor();
+ * param_1 := obj.Book(start,end);
+ */
+// @lc code=end
