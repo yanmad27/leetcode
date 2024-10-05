2302a6a438ff60a00371a98d17281c2762c2ea4b
DoanPhan <yanmad27@gmail.com>
2024-10-05 18:38:10 +0700

    solve: 567

---
+++ 567.permutation-in-string.go
@@ -0,0 +1,40 @@
+package main
+
+/*
+ * @lc app=leetcode id=567 lang=golang
+ *
+ * [567] Permutation in String
+ */
+
+// @lc code=start
+
+func checkInclusion(s1 string, s2 string) bool {
+	if len(s2) < len(s1) {
+		return false
+	}
+	for i := 0; i < len(s2)-len(s1)+1; i++ {
+		if check(s1, s2[i:i+len(s1)]) {
+			return true
+		}
+	}
+	return false
+}
+
+func check(s1, s2 string) bool {
+	s1Arr := make([]int, 26)
+	s2Arr := make([]int, 26)
+	for _, char := range s1 {
+		s1Arr[char-'a']++
+	}
+	for _, char := range s2 {
+		s2Arr[char-'a']++
+	}
+	for _, char := range s1 {
+		if s1Arr[char-'a'] != s2Arr[char-'a'] {
+			return false
+		}
+	}
+	return true
+}
+
+// @lc code=end
--- main.go
+++ main.go
@@ -4,6 +4,5 @@ import "fmt"
 
 func main() {
 
-	arg1 := []int{2, 3, 4, 2, 5, 5}
-	fmt.Println(dividePlayers(arg1))
+	fmt.Println(checkInclusion("adc", "dcda"))
 }
