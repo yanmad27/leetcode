ac2ed5e7dd986a05715d528cf4123db9a2e73e20
DoanPhan <yanmad27@gmail.com>
2024-10-12 08:41:09 +0700

    Merge branches 'main' and 'main' of github.com:yanmad27/leetcode

--- main.go
+++ main.go
@@ -4,9 +4,6 @@ import "fmt"
 
 func main() {
 
-<<<<<<< HEAD
 	fmt.Println(minSwaps("][]["))
-=======
 	fmt.Println(maxWidthRamp([]int{9, 8, 1, 0, 1, 9, 4, 0, 4, 1}))
->>>>>>> 049310a71da0004e90eb23bc8d8d85cc9a5bfb42
 }
