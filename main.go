c5a5f99a74ebf8dd6a1f1c8a6b76aa9f3a7729f1
DoanPhan <yanmad27@gmail.com>
2024-10-20 11:56:02 +0700

    solve: 1545

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import "fmt"
 
 func main() {
 
-	fmt.Println(countMaxOrSubsets([]int{3, 1}))
-	fmt.Println(maximumSwap(22341345))
+	fmt.Println(findKthBit(20, 1048575))
 	// fmt.Println(maxWidthRamp([]int{9, 8, 1, 0, 1, 9, 4, 0, 4, 1}))
 }
