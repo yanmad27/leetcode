98565f7955cc9e3db43a439f8f1e73ace9ba75f3
DoanPhan <doan.phan@silentium.io>
2024-10-23 09:10:43 +0700

    solve: 2583

---
+++ 2583.kth-largest-sum-in-a-binary-tree.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"sort"
+)
+
+/*
+ * @lc app=leetcode id=2583 lang=golang
+ *
+ * [2583] Kth Largest Sum in a Binary Tree
+ */
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func kthLargestLevelSum(root *TreeNode, k int) int64 {
+	sumMap := make(map[int]int)
+	var recursive func(root *TreeNode, level int)
+	recursive = func(root *TreeNode, level int) {
+		if root == nil {
+			return
+		}
+		sumMap[level] += root.Val
+		recursive(root.Left, level+1)
+		recursive(root.Right, level+1)
+	}
+	recursive(root, 0)
+	if k > len(sumMap) {
+		return -1
+	}
+	sumArr := []int{}
+	for _, sum := range sumMap {
+		sumArr = append(sumArr, sum)
+	}
+	sort.Ints(sumArr)
+	return int64(sumArr[len(sumArr)-k])
+}
+
+// @lc code=end
