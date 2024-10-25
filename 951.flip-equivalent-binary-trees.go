f5f5167aa603da6a428e5f4c1b91df9503ea0a71
DoanPhan <doan.phan@silentium.io>
2024-10-25 09:56:52 +0700

    solve: 951

--- 951.flip-equivalent-binary-trees.go
+++ 951.flip-equivalent-binary-trees.go
@@ -11,10 +11,40 @@ import "fmt"
 // @lc code=start
 func flipEquiv(root1 *TreeNode, root2 *TreeNode) bool {
 
-	if root1 == nil && root2 == nil {
-    return true
-  }
-  
+	path1Map := make(map[string]int)
+	path2Map := make(map[string]int)
+
+	var getPath func(tmp int, root *TreeNode, curPath string)
+	getPath = func(tmp int, root *TreeNode, curPath string) {
+		if root == nil {
+			if tmp == 1 {
+				path1Map[curPath]++
+			} else {
+				path2Map[curPath]++
+
+			}
+			return
+		}
+		curPath = fmt.Sprintf("%s_%d", curPath, root.Val)
+		getPath(tmp, root.Left, curPath)
+		getPath(tmp, root.Right, curPath)
+	}
+	getPath(1, root1, "")
+	getPath(2, root2, "")
+	for key1, val1 := range path1Map {
+		val2, ok := path2Map[key1]
+		if val2 != val1 || !ok {
+			return false
+		}
+	}
+	for key2, val2 := range path2Map {
+		val1, ok := path1Map[key2]
+		if val2 != val1 || !ok {
+			return false
+		}
+	}
+	return true
+
 }
 
 // @lc code=end
