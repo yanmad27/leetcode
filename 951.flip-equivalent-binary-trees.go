82a36b5fd880e86f512edd324e2dc87bc4281286
DoanPhan <doan.phan@silentium.io>
2024-10-25 09:15:49 +0700

    solve: 951

---
+++ 951.flip-equivalent-binary-trees.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+/*
+ * @lc app=leetcode id=951 lang=golang
+ *
+ * [951] Flip Equivalent Binary Trees
+ */
+
+// @lc code=start
+func flipEquiv(root1 *TreeNode, root2 *TreeNode) bool {
+
+	if root1 == nil && root2 == nil {
+    return true
+  }
+  
+}
+
+// @lc code=end
