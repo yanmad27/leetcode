56e33a0d393702c5c2481c0559b14f03a0aa66c7
DoanPhan <doan.phan@silentium.io>
2024-11-20 10:45:17 +0700

    solve: 2516

---
+++ 2516.take-k-of-each-character-from-left-and-right.go
@@ -0,0 +1,32 @@
+package main
+
+/*
+ * @lc app=leetcode id=2516 lang=golang
+ *
+ * [2516] Take K of Each Character From Left and Right
+ */
+
+// @lc code=start
+func takeCharacters(s string, k int) int {
+	count := make([]int, 3)
+	for _, c := range s {
+			count[c-'a']++
+	}
+	
+	if min(count[0], min(count[1], count[2])) < k {
+			return -1
+	}
+	res := len(s) + 1
+	l := 0
+	for r := 0; r < len(s); r++ {
+			count[s[r]-'a']--
+			for min(count[0], min(count[1], count[2])) < k {
+					count[s[l]-'a']++
+					l++
+			}
+			res = min(res, len(s)-(r-l+1))
+	}
+	return res
+}
+
+// @lc code=end
--- main.go
+++ main.go
@@ -3,5 +3,5 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(maximumSubarraySum([]int{9, 9, 9, 1, 2, 3}, 3))
+	fmt.Println(takeCharacters("cbbac", 1))
 }
