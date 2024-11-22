af4a52ac0029bce06de016500c972f7cfc50b49a
DoanPhan <doan.phan@silentium.io>
2024-11-22 10:52:35 +0700

    solve: 1072

--- main.go
+++ main.go
@@ -3,5 +3,5 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(countUnguarded(4, 6, [][]int{{0, 0}, {1, 1}, {2, 3}}, [][]int{{0, 1}, {2, 2}, {1, 4}}))
+	fmt.Println(maxEqualRowsAfterFlips([][]int{{0, 0, 1}, {0, 0, 1}, {1, 1, 0}}))
 }
