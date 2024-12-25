84b34d64f88349151d6ac9d72c8bd09927b6cb1e
DoanPhan <yanmad27@gmail.com>
2024-12-25 18:29:40 +0700

    solve: 515

---
+++ 515.find-largest-value-in-each-tree-row.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+/*
+ * @lc app=leetcode id=515 lang=golang
+ *
+ * [515] Find Largest Value in Each Tree Row
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func largestValues(root *TreeNode) []int {
+	res := []int{}
+	var travel func(*TreeNode, int)
+	travel = func(node *TreeNode, level int) {
+		if node == nil {
+			return
+		}
+		if level == len(res) {
+			res = append(res, node.Val)
+		} else {
+			if node.Val >= res[level] {
+				res[level] = node.Val
+			}
+		}
+		travel(node.Left, level+1)
+		travel(node.Right, level+1)
+	}
+	travel(root, 0)
+	return res
+}
+
+// @lc code=end
