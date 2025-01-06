5968ba889e501312267deb47c1837d8b55f52e93
DoanPhan <doan.phan@silentium.io>
2025-01-06 09:32:01 +0700

    solve: 1769

---
+++ 1769.minimum-number-of-operations-to-move-all-balls-to-each-box.go
@@ -0,0 +1,26 @@
+package main
+
+/*
+ * @lc app=leetcode id=1769 lang=golang
+ *
+ * [1769] Minimum Number of Operations to Move All Balls to Each Box
+ */
+
+// @lc code=start
+func minOperations(boxes string) []int {
+	hasBoxIndex := []int{}
+	for i := 0; i < len(boxes); i++ {
+		if boxes[i] == '1' {
+			hasBoxIndex = append(hasBoxIndex, i)
+		}
+	}
+	res := make([]int, len(boxes))
+	for i := 0; i < len(boxes); i++ {
+		for _, index := range hasBoxIndex {
+			res[i] += abs(index - i)
+		}
+	}
+	return res
+}
+
+// @lc code=end
