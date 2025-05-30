cbfb421bbb15a720f6af6a61aa9e6e4c98b1406e
DoanPhan <doan.phan@silentium.io>
2025-05-30 11:29:38 +0700

    solve: 1115

---
+++ 1115.print-foo-bar-alternately.go
@@ -0,0 +1,40 @@
+package main
+
+/*
+ * @lc app=leetcode id=1115 lang=golang
+ *
+ * [1115] Print FooBar Alternately
+ */
+
+// @lc code=start
+type FooBar struct {
+	n  int
+	ch chan int
+}
+
+func NewFooBar(n int) *FooBar {
+	return &FooBar{
+		n:  n,
+		ch: make(chan int),
+	}
+}
+
+func (fb *FooBar) Foo(printFoo func()) {
+	for i := 0; i < fb.n; i++ {
+		// printFoo() outputs "foo". Do not change or remove this line.
+		printFoo()
+		fb.ch <- 0
+		<-fb.ch
+	}
+}
+
+func (fb *FooBar) Bar(printBar func()) {
+	for i := 0; i < fb.n; i++ {
+		<-fb.ch
+		// printBar() outputs "bar". Do not change or remove this line.
+		printBar()
+		fb.ch <- 0
+	}
+}
+
+// @lc code=end
